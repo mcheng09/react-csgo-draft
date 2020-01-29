260cb0588c56dea5baac76555a85b50d67a2c126
Michael <mcheng.oc@gmail.com>
2020-01-29 00:18:50 -0800

    create(Layout): creates Layout component

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Aux from './../Aux/Aux'
+import Home from './../../pages/Home/Home'
+
+function Layout() {
+  return (
+    <Aux>
+      <Home />
+    </Aux>
+  )
+}
+
+export default Layout;
