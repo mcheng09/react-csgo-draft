50a55d7276f51ca5eba648f6079ff58641282357
Michael <mcheng.oc@gmail.com>
2020-01-29 00:19:00 -0800

    updates(App): adds Layout to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Layout from './hoc/Layout/Layout';
+
 function App() {
   return (
     <div className="App">
-
+      <Layout></Layout>
     </div>
   );
 }
