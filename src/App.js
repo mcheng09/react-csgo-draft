54d591b66a3c461f9f178786ac110a966d667b01
Michael <mcheng.oc@gmail.com>
2020-01-29 13:14:34 -0800

    feat(App): adds routes and paths to App level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Layout from './hoc/Layout/Layout';
+import HomePage from './pages/Home/Home';
+import PlayersPage from './pages/Players/Players'
+import ContactPage from './pages/Contact/Contact'
+
 
 function App() {
   return (
     <div className="App">
-      <Layout></Layout>
+      <BrowserRouter>
+        <Layout>
+            <Switch>
+              <Route path='/players' component={PlayersPage} />
+              <Route path='/contact' component={ContactPage} />
+              <Route path='/' component={HomePage} />
+            </Switch>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
