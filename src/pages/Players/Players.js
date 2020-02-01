e460ab36c372130e6cb115daff242048965b73a4
Michael <mcheng.oc@gmail.com>
2020-01-31 18:12:54 -0800

    feat(Players): adds basic pagination to project to cycle through players

--- src/pages/Players/Players.js
+++ src/pages/Players/Players.js
@@ -5,17 +5,32 @@ import classes from './Players.module.scss';
 
 function Players() {
   const [players, setPlayers] = useState([]);
+  const [pageOfPlayers, setPageOfPlayers] = useState([]);
+  const [currPage, setCurrPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(0);
+
+  const setPage = (currPage) => {
+    let allPlayers = [...players];
+    let spliceIndex = currPage * 10;
+    let newPlayers = allPlayers.splice(spliceIndex, 10);
+    setCurrPage(currPage);
+    setPageOfPlayers(newPlayers);
+  }
 
   useEffect(() => {
     axios.get('https://api.sportsdata.io/v3/csgo/scores/json/Players?key=47d152dbd924490bbf6f6d8fae797690')
       .then(function(res) {
-        const playerList = res.data.splice(0, 10);
+        const allPlayers = [...res.data];
+        const playerList = allPlayers.splice(0, 10);
         const updatedPlayers = playerList.map( player => {
           return {
             ...player
           }
         });
-        setPlayers(updatedPlayers);
+        const maxPages = Math.floor(res.data.length / 10);
+        setPlayers(res.data);
+        setPageOfPlayers(updatedPlayers);
+        setMaxPage(maxPages);
       })
       .catch(function(error) {
         console.log(error);
@@ -23,18 +38,21 @@ function Players() {
       .finally(function() {
         console.log('finally done');
       })
-  }, [])
+  }, []);
 
-  let eachPlayer = players.map((player, i) => {
-    console.log(player);
+  let eachPlayer = pageOfPlayers.map((player, i) => {
     return (
-      <p key={'player' + i}>Player Name: {player.FirstName} {player.LastName} - {player.BirthCountry}</p>
+      <p key={'player' + i}>Player Name: {player.CommonName} - {player.MatchName} - {player.BirthCountry}</p>
     )
   })
 
   return (
     <div className={classes.Players}>
       { eachPlayer }
+      <button onClick={() => setPage(0)} disabled={currPage < 1}>First</button>
+      <button onClick={() => setPage(currPage - 1)} disabled={currPage < 1}>Previous</button>
+      <button onClick={() => setPage(currPage + 1)} disabled={currPage >= maxPage}>Next</button>
+      <button onClick={() => setPage(maxPage)} disabled={currPage >= maxPage}>Last</button>
     </div>
   )
 }
