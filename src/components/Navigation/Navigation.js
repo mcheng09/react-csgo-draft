aa9a24f3880ff9c3a80204feddcd70d2a2158206
Michael <mcheng.oc@gmail.com>
2020-01-31 18:12:23 -0800

    update(Navigation): import NavigationItems component and removes redundant code

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import NavigationItems from './NavigationItems/NavigationItems'
+import classes from './Navigation.module.scss'
+
 function Navigation() {
   return (
-    <div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/players'>Player</Link>
-        <Link to='/contact'>Contact Us</Link>
-      </nav>
-    </div>
+    <nav className={classes.Navigation}>
+      <Link to='/' className={classes.Logo}>CSGO Draft</Link>
+      <NavigationItems />
+      <div></div>
+    </nav>
   )
 }
 
