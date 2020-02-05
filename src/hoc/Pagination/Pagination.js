7c9e9a1cb28f01ebfd09b605d86a6dbb9592cdd7
Michael <mcheng.oc@gmail.com>
2020-02-05 10:26:31 -0800

    feat(Pagination): adds numeral buttons based on maxIndex

--- src/hoc/Pagination/Pagination.js
+++ src/hoc/Pagination/Pagination.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 function Pagination (props) {
+
+  const numBtns = (num) => {
+    const btns = [];
+    for(let i = 0; i <= num; i++){
+      btns.push(<button
+        key={'button' + i}
+        onClick={() => props.click(i)} >{i + 1}</button>)
+    }
+    return btns;
+  }
+
   return (
     <div>
       <button onClick={() => props.click(0)} disabled={props.currIndex < 1}>First</button>
       <button onClick={() => props.click(props.currIndex - 1)} disabled={props.currIndex < 1}>Previous</button>
+      { numBtns(props.maxIndex) }
       <button onClick={() => props.click(props.currIndex + 1)} disabled={props.currIndex >= props.maxIndex}>Next</button>
       <button onClick={() => props.click(props.maxIndex)} disabled={props.currIndex >= props.maxIndex}>Last</button>
     </div>
