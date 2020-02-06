2efb3dea753b7dae484747634b42fc5cdf4c9d84
Michael <mcheng.oc@gmail.com>
2020-02-06 08:35:03 -0800

    debug(Pagination): adds condition if maxIndex is under 10

--- src/hoc/Pagination/Pagination.js
+++ src/hoc/Pagination/Pagination.js
@@ -6,19 +6,30 @@ function Pagination (props) {
 
   const numBtns = () => {
     const btns = [];
-    let first = 0;
-    if (props.currIndex < 4) first = 0
-    else if (props.currIndex > props.maxIndex - 5) first = props.maxIndex - 9;
-    else first = props.currIndex - 4;
+    if (props.maxIndex < 10) {
+      for (let i = 0; i < props.maxIndex; i++) {
+        btns.push(<button
+          key={'button' + i}
+          onClick={() => props.click(i)}
+          className={i === props.currIndex ? classes.active : null} >{i + 1}</button>)
+      }
+    }
+    else {
+      let first = 0;
+      if (props.currIndex < 4) first = 0
+      else if (props.currIndex > props.maxIndex - 5) first = props.maxIndex - 9;
+      else first = props.currIndex - 4;
 
-    for(let i = first; i < first + 10; i++){
-      btns.push(<button
-        key={'button' + i}
-        onClick={() => props.click(i)}
-        className={i === props.currIndex ? classes.active : null} >{i + 1}</button>)
+      for(let i = first; i < first + 10; i++){
+        btns.push(<button
+          key={'button' + i}
+          onClick={() => props.click(i)}
+          className={i === props.currIndex ? classes.active : null} >{i + 1}</button>)
+        }
     }
     return btns;
   }
+  console.log({props})
 
   return (
     <div className={classes.Pagination}>
