368790235e757a249ea88c3376db35c40db3d3b1
Michael <mcheng.oc@gmail.com>
2020-02-06 08:33:55 -0800

    feat(Teams): adds API call to sportsdata to bring in teams data

--- src/pages/Teams/Teams.js
+++ src/pages/Teams/Teams.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Team() {
+import Pagination from './../../hoc/Pagination/Pagination'
+import classes from './Teams.module.scss';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+  const [pageOfTeams, setPageOfTeams] = useState([]);
+  const [currPage, setCurrPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(0);
+
+  const setPage = (currPage) => {
+    let allTeams = {...teams};
+    setCurrPage(currPage);
+    setPageOfTeams(allTeams[currPage]);
+  }
+
+  useEffect(() => {
+    axios.get('https://api.sportsdata.io/v3/csgo/scores/json/Teams?key=47d152dbd924490bbf6f6d8fae797690')
+      .then(function(res) {
+        const allTeams = [...res.data];
+        const allTeamsMap = {};
+
+        allTeams.forEach((team, i) => {
+          let key = Math.floor(i / 10);
+          if (!allTeamsMap.hasOwnProperty(key)) allTeamsMap[key] = [team];
+          else allTeamsMap[key].push(team)
+        })
+        const maxPages = Math.floor(allTeams.length / 10);
+
+        setTeams(allTeamsMap);
+        setPageOfTeams(allTeamsMap[0]);
+        setMaxPage(maxPages);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+  }, []);
+
+  const eachTeam = pageOfTeams.map((team, i) => {
+    return (
+      <p key={'team' + i}>Team Name: {team.Name} - {team.AreaName} - {team.Founded}</p>
+    )
+  })
+
+
   return (
-    <div>Team Component</div>
+    <div className={classes.Teams}>
+      { eachTeam }
+      <Pagination
+        click={setPage}
+        currIndex={currPage}
+        maxIndex={maxPage} />
+    </div>
   )
 }
 
-export default Team;
+export default Teams;
